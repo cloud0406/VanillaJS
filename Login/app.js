2b2b30b2816c5d69d4be0317b098487001639a6d
김정호 <cloud0406@naver.com>
2021-07-06 22:15:50 +0900

    practice : login_form JS code

---
+++ Login/app.js
@@ -0,0 +1,11 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+
+function onLoginBtnClick() {
+    const username = loginInput.value;
+    console.log(username);
+}
+
+loginButton.addEventListener("click",onLoginBtnClick)
